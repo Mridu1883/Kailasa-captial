429e5b4ac0b7fe5e727e2c18bd106965a3333558
auximorus <nilesh111305@gmail.com>
2025-04-16 21:51:25 +0530

    b

--- strategy4_2.py
+++ strategy4_2.py
@@ -4,20 +4,20 @@ import matplotlib.pyplot as plt
 
 # Load your data
 df = pd.read_csv("NF_60.csv")
-df['datetime'] = pd.to_datetime(df['date'] + ' ' + df['time'])
+df['datetime'] = pd.to_datetime(df['Date'] + ' ' + df['Time'])
 
 def calc_atr(df,period = 14):
-    high = df['high']
-    low = df['low']
-    close = df['close']
+    high = df['High']
+    low = df['Low']
+    close = df['Close']
     sumoftr = 0 
     df["ATR"] = None
     for i in range(0,period):
-        sumoftr = df['high'].iloc[i]-df["low"].iloc[i]
+        sumoftr = df['High'].iloc[i]-df["Low"].iloc[i]
     df["ATR"].iloc[period-1] = sumoftr/period
     starter = 0
     for i in range(period,len(df)):
-        sumoftr = sumoftr - (df['high'].iloc[starter]-df["low"].iloc[starter]) + df['high'].iloc[i]-df["low"].iloc[i]
+        sumoftr = sumoftr - (df['High'].iloc[starter]-df["Low"].iloc[starter]) + df['High'].iloc[i]-df["Low"].iloc[i]
         starter = starter + 1
         df["ATR"].iloc[i] = sumoftr/period
 calc_atr(df,period=14)
