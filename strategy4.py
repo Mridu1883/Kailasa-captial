ff43505b479c1fde71050c31761b667b346f122d
auximorus <nilesh111305@gmail.com>
2025-04-16 19:12:37 +0530

    b

--- strategy4.py
+++ strategy4.py
@@ -67,6 +67,8 @@ def plot_renko(renko_df):
     plt.grid(True)
     plt.show()
 # Run everything
-brick_size = 10
+brick_size = 2
 renko_df = generate_renko(df, brick_size)
+print(renko_df.head())
+print(f"Renko bricks generated: {len(renko_df)}")
 plot_renko(renko_df)
