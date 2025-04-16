cf187fddce06dba3bedda6eb5e86f23c2d96ea48
auximorus <nilesh111305@gmail.com>
2025-04-16 21:47:52 +0530

    b

---
+++ strategy4_2.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Load your data
+df = pd.read_csv("NF_60.csv")
+df['datetime'] = pd.to_datetime(df['date'] + ' ' + df['time'])
+
+def calc_atr(df,period = 14):
+    high = df['high']
+    low = df['low']
+    close = df['close']
+    sumoftr = 0 
+    df["ATR"] = None
+    for i in range(0,period):
+        sumoftr = df['high'].iloc[i]-df["low"].iloc[i]
+    df["ATR"].iloc[period-1] = sumoftr/period
+    starter = 0
+    for i in range(period,len(df)):
+        sumoftr = sumoftr - (df['high'].iloc[starter]-df["low"].iloc[starter]) + df['high'].iloc[i]-df["low"].iloc[i]
+        starter = starter + 1
+        df["ATR"].iloc[i] = sumoftr/period
+calc_atr(df,period=14)
+print(df)
+
+
